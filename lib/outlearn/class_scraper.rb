1740bc393d130c635311b080c7f03fc1e6494df2
emill3000 <emill3000@yahoo.com>
2016-06-28 18:42:51 -0700

    scraping

--- lib/outlearn/class_scraper.rb
+++ lib/outlearn/class_scraper.rb
@@ -1,6 +1,16 @@
 class Outlearn::Class 
 
-  def scrape
+  def self.activities_list
+  	activities = []
+  	doc = Nokogiri::HTML(open('https://www.outsoul.com/'))
+  	doc.css('.activity-tile .h5').collect do |activity|
+  		activities = activity.text
+  	end	
   end
+
+  def self.places_list
+  	places = []
+  	doc = Nokogiri::HTML(open('https://www.outsoul.com/'))
+  	doc.css()
 
 end
--- lib/outlearn/cli.rb
+++ lib/outlearn/cli.rb
@@ -1,7 +1,25 @@
 class Outlearn::CLI
 
   def call
+  	start
+    list
 
+  end
+
+  def start
+  	puts "Welcome to Outlearn. How would you like to browse classes?"
+  end
+
+  def list
+  	puts "Type 1 for a list of activities or 2 for a list of places."
+  	input = gets.strip
+  	if input == "1"
+  		puts "What do you want to do?"
+  	elsif input == "2"
+  		puts "places"
+  	elsif 
+  		puts "I'm sorry. You can only choose 1 or 2. Please try again."
+  	end
   end
 
 end
