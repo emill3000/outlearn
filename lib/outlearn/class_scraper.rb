122a54a7ba77c31373432e299fd6e2e898617bdb
emill3000 <emill3000@yahoo.com>
2016-07-12 17:47:58 -0400

    making class scraper work

--- lib/outlearn/class_scraper.rb
+++ lib/outlearn/class_scraper.rb
@@ -1,27 +1,22 @@
+
 class Outlearn::Class 
   attr_accessor :name, :location, :description, :price, :date
 
-  def initialize
+  def initialize(name = nil, location = nil)
     @name = name
     @location = location
   end
   
-
   def self.activity_list
- 
-  	doc = Nokogiri::HTML(open('https://www.outsoul.com/'))
-  	activities = doc.css('.activity-tile')
-    activities.each do |activity| 
-      puts activity.css('.h5').text
-    end
+    doc = Nokogiri::HTML(open('https://www.outsoul.com/'))
+    activities = doc.css('.activity-tile .h5')
+    activities.collect{|activity| new(activity.text)}
   end
 
   def self.place_list
     doc = Nokogiri::HTML(open('https://www.outsoul.com/'))
-    places = doc.css('.area-tile')
-    places.each do |place|
-      puts place.css('.h5').text
-  	end	
-  end
-  
+    places = doc.css('.area-tile .h5')
+    places.collect{|place| new(place.text)}
+  end	
 end
+  
--- lib/outlearn/cli.rb
+++ lib/outlearn/cli.rb
@@ -3,26 +3,40 @@ class Outlearn::CLI
   def call
   	start
     list
-
+    describe
   end
 
   def start
   	puts "Welcome to Outlearn. How would you like to browse classes?"
+    list
   end
 
   def list
+  
+    #while input != "1" || "2"
     
-  	puts "Type 1 for a list of activities or 2 for a list of places."
-  	input = gets.strip
-  	if input == "1"
-  		Outlearn::Class.activity_list.each.with_index(1) do |activity, i|
-        puts "#{i}. #{activity}"
-      end
-  	elsif input == "2"
-  		Outlearn::Class.place_list
-  	elsif 
-  		puts "I'm sorry. You can only choose 1 or 2. Please try again."
-  	end
+  	puts "Type 1 for a list of activities or 2 for a list of places or exit to quit."
+      input = gets.strip
+      case input
+    	when "1"
+    		Outlearn::Class.activity_list.each.with_index(1) do |activity, i|
+          puts "#{i}. #{activity.name}"
+          #describe
+        end
+    	when "2"
+        Outlearn::Class.place_list.each.with_index(1) do |place, i|
+    		  puts "#{i}. #{place.name}"
+        end
+      else
+    		puts "I'm sorry. You can only choose 1 or 2. Please try again."
+        #list 
+    	end
+    end
+  #end
+
+  def describe
+    puts "choose a number"
+
   end
 
 end
