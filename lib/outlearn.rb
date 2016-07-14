0f15216421c36f654ea8cb148a38f59a7db0c8eb
emill3000 <emill3000@yahoo.com>
2016-07-14 11:50:09 -0400

    scraping

--- lib/outlearn.rb
+++ lib/outlearn.rb
@@ -3,8 +3,9 @@ require "pry"
 require "open-uri"
 
 require_relative "outlearn/version"
-require_relative "outlearn/class_scraper"
+require_relative "outlearn/activities"
 require_relative "outlearn/cli"
+require_relative "outlearn/classes"
 
 module Outlearn
 end
