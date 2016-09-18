121d49b6db888924136f3524201282ea3c93a83b
emill3000 <emill3000@yahoo.com>
2016-09-17 17:05:08 -0700

    better cli

--- lib/outlearn/cli.rb
+++ lib/outlearn/cli.rb
@@ -3,25 +3,22 @@ class Outlearn::CLI
   def call
     Outlearn::Scraper.new.make_classes
     start
-    class_list
-    print_classes(activity)
   end
 
   def start
     puts "Welcome to Outlearn!"
+
 		Outlearn::Scraper.new.name_list.each.with_index(1) do |activity, i|
       puts "#{i}. #{activity}"  
     end
-    
-  end
 
-  def class_list
-    puts "choose a number for more information on an activity."
+    puts "Choose a number for more information about an activity."
     input = gets.strip
     activity = Outlearn::Classes.find(input.to_i)
     print_classes(activity)
 
   end
+
 
   def print_classes(activity)
     Outlearn::Classes.all.each.with_index(1) do |c, i|
